16d64caeed772dc4f1b5eed83dc357907bd7cf99
William Walker <w_walker@icloud.com>
2022-03-17 12:52:30 +0000

    v2 beta

--- worker.js
+++ worker.js
@@ -64,7 +64,52 @@ th, td {
 </style>
 `;
 
-const board = (flags) => `
+const flagPage = (flag) => `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Operation Mad Duck | ${flag.name} Flag</title>
+    ${style}
+  </head>
+  <body>
+    <div class="container">
+        <div class="subcontainer">
+          <h1>${flag.name} Flag</h1>
+          <h2>Capture!</h2>
+        </div>
+    </div>
+  </body>
+  <script>
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get('id')
+    window.history.replaceState(null, "", "/");
+    var captureFlag = (contract) => {
+      if (contract) {
+        fetch("/capture?id=" + id, { method: "POST", body: contract })
+        .then((response) => {
+          if (response.ok) {
+            alert("Contract statement issued successfully");
+          } else if (response.status === 403) {
+            alert("This flag has already been captured!")
+          } else {
+            alert("HTTP Error " + response.status + ". Please try again.");
+          }
+        })
+      }
+    }
+    var requestContract = (_event) => {
+      let contract = prompt("Please enter your team's contract:");
+      captureFlag(contract)
+    }
+    document.querySelector("h2").addEventListener("click", requestContract)
+  </script>
+</html>
+`;
+
+const boardPage = (flags) => `
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -145,7 +190,7 @@ const board = (flags) => `
 </html>
 `;
 
-const reset = `
+const resetPage = `
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -164,7 +209,7 @@ const reset = `
   <script>
     var reset = (confirmation) => {
       if (confirmation === 'RESET') {
-        fetch("/reset", { method: "DELETE", body: confirmation })
+        fetch("/reset", { method: "POST", body: confirmation })
         .then( (response) => {
           if (response.ok) {
             alert("The score board has been reset!")
@@ -183,117 +228,44 @@ const reset = `
 </html>
 `;
 
-const banner = (flag) => `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Operation Mad Duck | ${flag.name} Flag</title>
-    ${style}
-  </head>
-  <body>
-    <div class="container">
-        <div class="subcontainer">
-          <h1>${flag.name} Flag</h1>
-          <h2>Capture!</h2>
-        </div>
-    </div>
-  </body>
-  <script>
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const id = urlParams.get('id')
-    window.history.replaceState({}, document.title, "/");
-    var captureFlag = (contract) => {
-      if (contract) {
-        fetch("/flag?id=" + id, { method: "POST", body: contract })
-        .then((response) => {
-          if (response.ok) {
-            alert("Contract statement issued successfully");
-          } else if (response.status === 403) {
-            alert("This flag has already been captured!")
-          } else {
-            alert("HTTP Error " + response.status + ". Please try again.");
-          }
-        })
-      }
-    }
-    var requestContract = (_event) => {
-      let contract = prompt("Please enter your team's contract:");
-      captureFlag(contract)
-    }
-    document.querySelector("h2").addEventListener("click", requestContract)
-  </script>
-</html>
-`;
-
-async function getScoreBoard(_request) {
-  const promises = [];
-
-  for (const key of Array(18).keys()) {
-      promises.push(FLAGS.get((key + 1).toString(), {type: "json"}))
+async function getFlag(request) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+  const flag = await FLAGS.get(id.toString(), { type: "json" });
+  if (flag === null) {
+    return new Response("The requested resource could not be found. 🦆", {
+      status: 404,
+    });
   }
 
-  const data = await Promise.all(promises)
-  const body = board(JSON.stringify(data));
+  const body = flagPage(flag);
   return new Response(body, {
     headers: { "Content-Type": "text/html" },
   });
 }
 
-async function init(contract) {
-  if (contract === "RESET") {
-    await FLAGS.put("1", '{"name":"Broncos", "times":[], "contracts":[], "red":800, "blue":800, "winner":null}')
-    await FLAGS.put("2", '{"name":"Buccaneers", "times":[], "contracts":[], "red":0, "blue":1200, "winner":null}')
-    await FLAGS.put("3", '{"name":"Chargers", "times":[], "contracts":[], "red":400, "blue":400, "winner":null}')
-    await FLAGS.put("4", '{"name":"Chiefs", "times":[], "contracts":[], "red":800, "blue":200, "winner":null}')
-    await FLAGS.put("5", '{"name":"Commanders", "times":[], "contracts":[], "red":200, "blue":200, "winner":null}')
-    await FLAGS.put("6", '{"name":"Cowboys", "times":[], "contracts":[], "red":400, "blue":400, "winner":null}')
-    await FLAGS.put("7", '{"name":"Dolphins", "times":[], "contracts":[], "red":600, "blue":400, "winner":null}')
-    await FLAGS.put("8", '{"name":"Eagles", "times":[], "contracts":[], "red":1200, "blue":600, "winner":null}')
-    await FLAGS.put("9", '{"name":"Giants", "times":[], "contracts":[], "red":600, "blue":600, "winner":null}')
-    await FLAGS.put("10", '{"name":"Jaguars", "times":[], "contracts":[], "red":400, "blue":400, "winner":null}')
-    await FLAGS.put("11", '{"name":"Jets", "times":[], "contracts":[], "red":200, "blue":600, "winner":null}')
-    await FLAGS.put("12", '{"name":"Patriots", "times":[], "contracts":[], "red":800, "blue":200, "winner":null}')
-    await FLAGS.put("13", '{"name":"Ravens", "times":[], "contracts":[], "red":200, "blue":800, "winner":null}')
-    await FLAGS.put("14", '{"name":"Saints", "times":[], "contracts":[], "red":800, "blue":200, "winner":null}')
-    await FLAGS.put("15", '{"name":"Seahawks", "times":[], "contracts":[], "red":200, "blue":200, "winner":null}')
-    await FLAGS.put("16", '{"name":"Texans", "times":[], "contracts":[], "red":200, "blue":600, "winner":null}')
-    await FLAGS.put("17", '{"name":"Titans", "times":[], "contracts":[], "red":800, "blue":200, "winner":null}')
-    await FLAGS.put("18", '{"name":"Vikings", "times":[], "contracts":[], "red":200, "blue":800, "winner":null}')
-    return new Response(null, { status: 200 });
-  }
-}
-
-async function getFlag(id) {
-  const flag = await FLAGS.get(id.toString(), {type: "json"})
-  if (flag === null) {
-    return new Response("The requested flag does not exist!", { status: 404 });
-  }
-
-  const body = banner(flag);
-  return new Response(body, {
-    headers: { "Content-Type": "text/html" }
-  });
-}
-
-async function captureFlag(contract, id) {
+async function captureFlag(request) {
   try {
-    const flag = await FLAGS.get(id, {type: "json"})
-    console.log(flag)
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+    const contract = await request.text();
+    const flag = await FLAGS.get(id, { type: "json" });
+    console.log(flag);
     if (flag.winner) {
-      return new Response(null, { status: 403 })
+      return new Response(null, { status: 403 });
     } else {
-      const winner = await check(contract, id)
-      await FLAGS.put(id, JSON.stringify({
-        name: flag.name, 
-        times: flag.times.concat((new Date).toTimeString().split(" ")[0]), 
-        contracts: flag.contracts.concat(contract), 
-        red: flag.red, 
-        blue: flag.blue, 
-        winner: winner
-      }))
+      const winner = await check(contract, id);
+      await FLAGS.put(
+        id,
+        JSON.stringify({
+          name: flag.name,
+          times: flag.times.concat(new Date().toTimeString().split(" ")[0]),
+          contracts: flag.contracts.concat(contract),
+          red: flag.red,
+          blue: flag.blue,
+          winner: winner,
+        })
+      );
       return new Response(null, { status: 200 });
     }
   } catch (err) {
@@ -302,37 +274,138 @@ async function captureFlag(contract, id) {
 }
 
 async function check(contract, id) {
-  const flag = await FLAGS.get(id, {type: "json"})
-  const redExp = new RegExp(`Red HQ [\\s\\S]*? Touchdown ${flag.name}`, "i")
-  const blueExp = new RegExp(`Blue HQ [\\s\\S]*? Touchdown ${flag.name}`, "i")
+  const flag = await FLAGS.get(id, { type: "json" });
+  const redExp = new RegExp(`Red HQ [\\s\\S]*? Touchdown ${flag.name}`, "i");
+  const blueExp = new RegExp(`Blue HQ [\\s\\S]*? Touchdown ${flag.name}`, "i");
 
   if (redExp.test(contract)) {
-    console.log("Valid Red Contract!")
-    return "red"
+    console.log("Valid Red Contract!");
+    return "red";
   } else if (blueExp.test(contract)) {
-    console.log("Valid Blue Contract!")
-    return "blue"
+    console.log("Valid Blue Contract!");
+    return "blue";
   } else {
-    console.log("Invalid Contract!!!")
-    return null
+    console.log("Invalid Contract!!!");
+    return null;
+  }
+}
+
+async function getBoard(_request) {
+  const promises = [];
+
+  for (const key of Array(18).keys()) {
+    promises.push(FLAGS.get((key + 1).toString(), { type: "json" }));
+  }
+
+  const data = await Promise.all(promises);
+  const body = boardPage(JSON.stringify(data));
+  return new Response(body, {
+    headers: { "Content-Type": "text/html" },
+  });
+}
+
+async function resetBoard(request) {
+  if (request.method === "POST") {
+    const confirmation = await request.text();
+    if (confirmation === "RESET") {
+      await FLAGS.put(
+        "1",
+        '{"name":"Broncos", "times":[], "contracts":[], "red":800, "blue":800, "winner":null}'
+      );
+      await FLAGS.put(
+        "2",
+        '{"name":"Buccaneers", "times":[], "contracts":[], "red":0, "blue":1200, "winner":null}'
+      );
+      await FLAGS.put(
+        "3",
+        '{"name":"Chargers", "times":[], "contracts":[], "red":400, "blue":400, "winner":null}'
+      );
+      await FLAGS.put(
+        "4",
+        '{"name":"Chiefs", "times":[], "contracts":[], "red":800, "blue":200, "winner":null}'
+      );
+      await FLAGS.put(
+        "5",
+        '{"name":"Commanders", "times":[], "contracts":[], "red":200, "blue":200, "winner":null}'
+      );
+      await FLAGS.put(
+        "6",
+        '{"name":"Cowboys", "times":[], "contracts":[], "red":400, "blue":400, "winner":null}'
+      );
+      await FLAGS.put(
+        "7",
+        '{"name":"Dolphins", "times":[], "contracts":[], "red":600, "blue":400, "winner":null}'
+      );
+      await FLAGS.put(
+        "8",
+        '{"name":"Eagles", "times":[], "contracts":[], "red":1200, "blue":600, "winner":null}'
+      );
+      await FLAGS.put(
+        "9",
+        '{"name":"Giants", "times":[], "contracts":[], "red":600, "blue":600, "winner":null}'
+      );
+      await FLAGS.put(
+        "10",
+        '{"name":"Jaguars", "times":[], "contracts":[], "red":400, "blue":400, "winner":null}'
+      );
+      await FLAGS.put(
+        "11",
+        '{"name":"Jets", "times":[], "contracts":[], "red":200, "blue":600, "winner":null}'
+      );
+      await FLAGS.put(
+        "12",
+        '{"name":"Patriots", "times":[], "contracts":[], "red":800, "blue":200, "winner":null}'
+      );
+      await FLAGS.put(
+        "13",
+        '{"name":"Ravens", "times":[], "contracts":[], "red":200, "blue":800, "winner":null}'
+      );
+      await FLAGS.put(
+        "14",
+        '{"name":"Saints", "times":[], "contracts":[], "red":800, "blue":200, "winner":null}'
+      );
+      await FLAGS.put(
+        "15",
+        '{"name":"Seahawks", "times":[], "contracts":[], "red":200, "blue":200, "winner":null}'
+      );
+      await FLAGS.put(
+        "16",
+        '{"name":"Texans", "times":[], "contracts":[], "red":200, "blue":600, "winner":null}'
+      );
+      await FLAGS.put(
+        "17",
+        '{"name":"Titans", "times":[], "contracts":[], "red":800, "blue":200, "winner":null}'
+      );
+      await FLAGS.put(
+        "18",
+        '{"name":"Vikings", "times":[], "contracts":[], "red":200, "blue":800, "winner":null}'
+      );
+      return new Response(null, { status: 200 });
+    }
+  } else {
+    return new Response(resetPage, {
+      headers: { "Content-Type": "text/html" },
+    });
   }
 }
 
 async function handleRequest(request) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get("id")
-  if (request.method === "POST") {
-    const contract = await request.text();
-    return captureFlag(contract, id);
-  } else if (request.method === "DELETE") {
-    const contract = await request.text();
-    return init(contract);
-  } else if (request.url.includes("flag")) {
-    return getFlag(id);
-  } else if (request.url.includes("reset")) {
-    return new Response(reset, { headers: { "Content-Type": "text/html" } });
-  } else {
-    return getScoreBoard(request);
+  const url = new URL(request.url);
+  const path = url.pathname;
+
+  switch (path) {
+    case "/flag":
+      return getFlag(request);
+    case "/capture":
+      return captureFlag(request);
+    case "/board":
+      return getBoard(request);
+    case "/reset":
+      return resetBoard(request);
+    default:
+      return new Response("The requested resource could not be found. 🦆", {
+        status: 404,
+      });
   }
 }
 
